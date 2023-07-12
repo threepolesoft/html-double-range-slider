ffe5d61bca5c1107d6210fdb3494c15e85b5a836
threepolesoft <threepolesoft@gmail.com>
2023-07-12 11:22:25 +0600

    change class name

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let sliderOne = document.getElementById("slider-1");
 let sliderTwo = document.getElementById("slider-2");
 let displayValOne = document.getElementById("range1");
 let displayValTwo = document.getElementById("range2");
-let minGap = 5;
+let minGap = 10;
 let sliderTrack = document.querySelector(".slider-track");
 
 let sliderMaxValue = document.getElementById("slider-2").max;
